851bbbfbc09d8c2b8014144ec823cb6a4224b0c6
Saurav Singh <97401461+saurav-22@users.noreply.github.com>
2025-09-20 21:27:54 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ module "vpc" {
   azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
-  database_subnets = ["10.1.201.0/24"]
+  database_subnets = ["10.1.201.0/24", "10.1.202.0/24"]
 
   create_database_subnet_group = true
 
